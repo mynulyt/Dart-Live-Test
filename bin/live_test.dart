fe2778e311e55812bcbb899fed425c8e21c8a56b
Mynul Alam <mynulalam27@gmail.com>
2025-06-25 23:07:17 +0600

    c

--- bin/live_test.dart
+++ bin/live_test.dart
@@ -1,1 +1,16 @@
-
+void main() {
+  List<Map<String, dynamic>> students = [
+    {
+      "name": "Alice",
+      "scores": [85, 90, 78],
+    },
+    {
+      "name": "Bob",
+      "scores": [85, 90, 78],
+    },
+    {
+      "name": " Alice",
+      "scores": [85, 90, 78],
+    },
+  ];
+}
