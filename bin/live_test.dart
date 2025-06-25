f47412b066526a58d6df1a3693f3a37ebadac390
Mynul Alam <mynulalam27@gmail.com>
2025-06-25 23:14:57 +0600

    c

--- bin/live_test.dart
+++ bin/live_test.dart
@@ -13,4 +13,16 @@ void main() {
       "scores": [90, 92, 85],
     },
   ];
+
+  //For calculate average scores
+  Map<String, double> averageScores = {};
+  for (var student in students) {
+    String name = student["name"];
+    List<int> scores = List<int>.from(student["scores"]);
+
+    double avg = scores.reduce((a, b) => a + b) / scores.length;
+    averageScores[name] = double.parse(avg.toStringAsFixed(2));
+  }
+
+  ///
 }
