8450f49dfc8e39bec434f238444253d1056d8ccf
Mynul Alam <mynulalam27@gmail.com>
2025-06-25 23:23:10 +0600

    c

--- bin/live_test.dart
+++ bin/live_test.dart
@@ -14,7 +14,6 @@ void main() {
     },
   ];
 
-  //For calculate average scores
   Map<String, double> averageScores = {};
   for (var student in students) {
     String name = student["name"];
@@ -24,10 +23,13 @@ void main() {
     averageScores[name] = double.parse(avg.toStringAsFixed(2));
   }
 
-  //
   var sortedEntries =
       averageScores.entries.toList()
         ..sort((a, b) => b.value.compareTo(a.value));
 
-  //
+  Map<String, double> sortedMap = {
+    for (var entry in sortedEntries) entry.key: entry.value,
+  };
+
+  print(sortedMap);
 }
